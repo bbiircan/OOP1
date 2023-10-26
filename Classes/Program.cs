b0574f45800533b5488facea785ebac0d2e92274
bbiircan <127721072+bbiircan@users.noreply.github.com>
2023-10-26 17:25:55 +0300

    Add files via upload

---
+++ Classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Bursa";
+            customer.Id = 1;
+            customer.LastName = "Bircan";
+            customer.FirstName = "Rümeysa";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2, City = "İstanbul", FirstName = "Eylül Ada", LastName = "Bircan"
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //InterfacesIntro();
+
+            //interface ve abstractta new anahtar sözcüğü ile nesneleri oluşturulamaz.
+
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[3]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+                new MySqlCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new OracleCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            Customer customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Rümeysa",
+                LastName = "Bircan",
+                Address = "Bursa"
+            };
+
+            Student student = new Student()
+            {
+                Id = 1,
+                FirstName = "Eylül Ada",
+                LastName = "Bircan",
+                Departmant = "Software Developer"
+            };
+
+            manager.Add(customer);
+            manager.Add(student);
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    class Worker : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Departmant { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName); //nesne de parametre olabilir
+        }
+    }
+}
